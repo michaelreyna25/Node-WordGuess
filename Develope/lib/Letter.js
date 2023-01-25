7f0b66bf24d8e4b43524081043ac5224c06ea304
Ian Senne <ianssenne@gmail.com>
2023-01-25 13:56:53 -0800

    add implimentation

--- Develope/lib/Letter.js
+++ Develope/lib/Letter.js
@@ -1,5 +1,23 @@
 class Letter {
-
+	constructor(char){
+		this.char = char;
+		this.visible = /[^a-z0-9]/i.test(this.char);
+		this.guessed = this.visible;
+	}
+	toString(){
+		return this.visible ? this.char : "_"
+	}
+	guess(letter){
+		if(this.char === letter){
+			this.guessed = true;
+			this.visible = true;
+			return true;
+		}
+		return false;
+	}
+	getSolution(){
+		return this.char;
+	}
 }
 
 module.exports = Letter;
--- Develope/lib/Word.js
+++ Develope/lib/Word.js
@@ -1,7 +1,23 @@
 const Letter = require("./Letter");
 
 class Word {
-  
+	constructor(word) {
+		this.word = word;
+		this.letters = word.split('').map(letter => new Letter(letter));
+	}
+	guessLetter(char) {
+		let hasGuessedALetter = false
+		this.letters.forEach(letter => {
+			hasGuessedALetter = letter.guess(char) ? true : hasGuessedALetter
+		});
+		return hasGuessedALetter;
+	}
+	guessedCorrectly() {
+		return !this.letters.find((letter)=>!letter.guessed)
+	}
+	toString() {
+		return this.letters.map(_=>_.toString())
+	}
 }
 
 module.exports = Word;
